fe3bc7221e963931da0cf1cb38f7ed7862ce8598
Li Li <li.li@gritacademy.se>
2024-04-29 22:21:58 +0200

    Create products & Main menu
    Create products, upload product images, create main_menu, main_icons menu in dashboard.

--- wp-content/themes/mytheme/init.php
+++ wp-content/themes/mytheme/init.php
@@ -6,3 +6,21 @@ function mytheme_enqueue()
     wp_enqueue_style("custom-style", $theme_directory_uri . "/style.css");
 }
 add_action("wp_enqueue_scripts", "mytheme_enqueue");
+
+function mytheme_init()
+{
+    // add theme support
+    add_theme_support('post-thumbnails');
+
+    // register MENU
+    $menu = array(
+        'main_menu' => 'main_menu',
+        'main_menu_icons' => 'main_menu_icons',
+        'footer_social_media' => 'footer_social_media',
+        'footer_shopping' => 'footer_shopping',
+        'footer_links' => 'footer_links',
+        'footer_blog' => 'footer_blog'
+    );
+    register_nav_menus($menu);
+}
+add_action("after_setup_theme", "mytheme_init");
