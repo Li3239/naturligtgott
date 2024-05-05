866385bc07ff6662c1983fe20aaac04a416e9636
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-05-05 03:10:48 +0200

    Created and styled header. Added icons etc.

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -1,12 +1,50 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title><?=get_option('blogname');?></title>
     <?php wp_head(); ?>
 </head>
 
 <body>
     <?php wp_body_open(); ?>
 
-    <header>
+    <header class="header">
+        <div class="logo-div">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/resources/images/logo.png'; ?>" alt="Logo">
+        </div>
+        <div class="main-menu-div">
+        <?php 
+            $menu = array(
+                'theme_location' => 'main_menu',
+                'menu_id' => 'primary-menu',
+                'container' => 'nav',
+                'container_class' => 'menu'
+            );
+            
+            wp_nav_menu($menu); ?> 
+        </div>
+        <div class="menu-logo-div">
+        <?php 
+            $menu = array(
+                'theme_location' => 'main_menu_icons',
+                'menu_id' => 'secondary-menu',
+                'container' => 'nav',
+                'container_class' => 'menu'
+            );
+            
+            wp_nav_menu($menu); ?> 
+        </div>
        
     </header>
+    <div class="subheader">
+        <a href=""><h4>ALLA PRODUKTER</h4></a>
+        <a href=""><h4>BÄST SÄLJARE</h4></a>
+        <a href=""><h4>NYHETER</h4></a>
+        <a href=""><h4>EKOLOGIST</h4></a>
+        <a href=""><h4>HÅLLBARA PRODUKTER</h4></a>
+        <a href=""><h4>REA</h4></a>
+    </div>
