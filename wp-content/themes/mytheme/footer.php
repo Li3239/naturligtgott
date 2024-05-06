b9540c90a55f82f91e6579db89cd1c99dbf8de8e
Li Li <li.li@gritacademy.se>
2024-05-06 13:25:57 +0200

    Footer

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -1,5 +1,50 @@
 <footer>
+    <section class="container">
+        <div class="column-address">
+            <a href="">
+                <img class="footer-image" src="<?= get_option('store_footer_image'); ?>" />
+            </a>
+            <div class="footer-info">
+                <span><?= get_option('store_footer_name'); ?></span>
+                <span><?= get_option('store_footer_copyright'); ?></span>
+                <span><?= get_option('store_footer_org_number'); ?></span>
+            </div>
+        </div>
 
+        <div class="column-contact">
+            <?php
+            $menu = array(
+                'theme_location' => 'footer_contact',
+                'menu_id' => 'footer_contact',
+                'container' => 'nav',
+                'container_class' => 'menu'
+            );
+            wp_nav_menu($menu);
+            ?>
+        </div>
+        <div class="column-product">
+            <?php
+            $menu = array(
+                'theme_location' => 'footer_product',
+                'menu_id' => 'footer_product',
+                'container' => 'nav',
+                'container_class' => 'menu'
+            );
+            wp_nav_menu($menu);
+            ?>
+        </div>
+        <div class="column-social-media">
+            <?php
+            $menu = array(
+                'theme_location' => 'footer_social_media',
+                'menu_id' => 'footer_social_media',
+                'container' => 'nav',
+                'container_class' => 'menu'
+            );
+            wp_nav_menu($menu);
+            ?>
+        </div>
+    </section>
 </footer>
 <?php wp_footer(); ?>
 </body>
