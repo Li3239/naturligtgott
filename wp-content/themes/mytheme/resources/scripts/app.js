f069b6f4fc0dd9c9571a6c666fb3b7f0bab6769d
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-05-09 19:59:53 +0200

    Continued styling header and home, created a mobile menu, and added responsiveness to mobile and pad

--- wp-content/themes/mytheme/resources/scripts/app.js
+++ wp-content/themes/mytheme/resources/scripts/app.js
@@ -1,1 +1,17 @@
-// import 'path/to/somejavascript.js';
+document.addEventListener('DOMContentLoaded', function() {
+    var burgerMenuDiv = document.querySelector('.burger-menu-div');
+    var mobileMenu = document.querySelector('.mobile-menu');
+
+    // Open mobile menu when burger menu is clicked
+    burgerMenuDiv.addEventListener('click', function(event) {
+        mobileMenu.classList.toggle('show');
+        event.stopPropagation(); // Prevents the click event from reaching the document body
+    });
+
+    // Close mobile menu when clicking outside of it
+    document.body.addEventListener('click', function(event) {
+        if (!mobileMenu.contains(event.target) && !burgerMenuDiv.contains(event.target)) {
+            mobileMenu.classList.remove('show');
+        }
+    });
+});
