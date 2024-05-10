4d2d63dc792de36b7f93432f0d05039ba77294c0
MrGauws <120020363+MrGauws@users.noreply.github.com>
2024-05-10 17:44:38 +0200

    added the image breadcrumb arrow to uploads\04 and added the hook to change it from standard

--- wp-content/themes/mytheme/hooks.php
+++ wp-content/themes/mytheme/hooks.php
@@ -1,2 +1,13 @@
 <?php
 
+// CHANGES BREADCRUMB DELIMITER
+function change_breadcrumb_delimiter( $defaults ) {
+    // GET URL FOR THE ICON FROM MEDIA LIBRARY
+    $icon_url = 'http://naturligtgott.test/wp-content/uploads/2024/05/breadcrumbarrow.png';
+
+    // REPLACE DELIMITER WITH ICON
+    $defaults['delimiter'] = '<img src="' . esc_url( $icon_url ) . '" alt="Breadcrumb Icon" class="breadcrumb-icon">';
+
+    return $defaults;
+}
+add_filter( 'woocommerce_breadcrumb_defaults', 'change_breadcrumb_delimiter' );
