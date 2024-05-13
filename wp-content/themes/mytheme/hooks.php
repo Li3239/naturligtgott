d5ceb61c5ea685519b638eb8f6b953bbbea9525f
MrGauws <120020363+MrGauws@users.noreply.github.com>
2024-05-13 22:07:46 +0200

    Added seperators, for some reason the first one wouldnt work so I used Jquery instead. Made description to z-index: 999 because it felt easy at this moment, might change this later on. Group pics was affecting the tabs. Changed color of text in tabs, moved around buy btn.

--- wp-content/themes/mytheme/hooks.php
+++ wp-content/themes/mytheme/hooks.php
@@ -10,3 +10,25 @@ function change_breadcrumb_delimiter( $defaults ) {
 
     return $defaults;
 }
+
+
+add_action('woocommerce_after_single_product_summary', 'add_custom_separator_before_sku', 20);
+
+function add_custom_separator_before_sku() {
+    ?>
+    <script>
+        jQuery(function($) {
+            $(document).ready(function() {
+                $('.sku_wrapper').before('<div style="width: 405px; height: 1px; background-color: #D9D9D9; margin-bottom: 20px;"></div>');
+            });
+        });
+    </script>
+    <?php
+}
+add_action( 'woocommerce_product_meta_end', 'add_custom_separator_after_product_meta' );
+
+function add_custom_separator_after_product_meta() {
+    ?>
+    <div class="custom-separator"></div>
+    <?php
+}
