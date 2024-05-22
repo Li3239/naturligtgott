39b5db30936f208113f6a437f3cbe5eafd49d315
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-05-22 17:16:25 +0200

    Added reviews and stars for reviews. Changed button texts to swedish with hooks

--- wp-content/themes/mytheme/hooks.php
+++ wp-content/themes/mytheme/hooks.php
@@ -34,3 +34,40 @@ function add_custom_separator_after_product_meta() {
     <div class="custom-separator"></div>
     <?php
 }
+
+// Change text on Home page on Add to cart btn
+function custom_woocommerce_product_add_to_cart_text() {
+    return __( 'Lägg till', 'woocommerce' ); // Change 'Custom Text' to your desired button text
+}
+add_filter( 'woocommerce_product_add_to_cart_text', 'custom_woocommerce_product_add_to_cart_text' ); // For product archives
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'custom_woocommerce_product_add_to_cart_text' ); // For single product pages
+
+add_action( 'woocommerce_after_shop_loop_item_title', 'add_custom_rating_pro', 5 );
+
+function add_custom_rating_pro() {
+    global $product;
+
+    if ( $product->get_review_count() > 0 ) {
+        $average_rating = $product->get_average_rating();
+        $rating_count = $product->get_review_count();
+
+        $output = '<div class="woocommerce-product-rating">';
+
+        for ($i = 1; $i <= 5; $i++) {
+     
+            if ($i <= $average_rating) {
+            
+                $output .= '<img src="' . get_template_directory_uri() . '/resources/images/full_star.png" alt="fullstar">';
+            } else if ($i - 0.5 <= $average_rating) {
+            
+                $output .= '<img src="' . get_template_directory_uri() . '/resources/images/half_star.png" alt="halfstar">';
+            } else {
+         
+                $output .= '<img src="' . get_template_directory_uri() . '/resources/images/empty_star.png" alt="emptystar">';
+            }
+        }
+
+        $output .= '</div>';
+        echo $output;
+    }
+}
